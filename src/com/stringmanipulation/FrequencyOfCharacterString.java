54b522af90ea17ac5fe4fd476c77d28a1e60cc79
Amul <95516383+bob-5757@users.noreply.github.com>
2023-03-31 21:46:53 +0530

    minor changes in frequency of character

--- src/com/stringmanipulation/FrequencyOfCharacterString.java
+++ src/com/stringmanipulation/FrequencyOfCharacterString.java
@@ -1,18 +1,21 @@
 package com.stringmanipulation;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FrequencyOfCharacterString {
 
 	public static void main(String[] args) {
 
-		String str = "aabbcc  dd ee nn ff";
-		// a a
+		String str = "aabbacc dd ee zzzz znn ff";
+		str = str.replaceAll(" ", "");
+		char thatchar = 0;
+		int charCount = 0;
 
 		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
 
-		for (int i = 0; i < str.length(); i++) 
-		{
+		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 			if (hm.containsKey(c)) {
 				hm.put(c, hm.get(c) + 1);
@@ -22,8 +25,14 @@ public class FrequencyOfCharacterString {
 
 		}
 
-		System.out.println(hm);
+		for (Map.Entry<Character, Integer> entry : hm.entrySet()) {// a 5 b 6
+			if (entry.getValue() > charCount) {
+				thatchar = entry.getKey();
+				charCount = entry.getValue();
+			}
+		}
 
+		System.out.println(thatchar + " occured " + charCount + " times");
 	}
 
 }
